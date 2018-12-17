ed6635abdfbdfc3d8a9401cda9b9f3f8eb07a44e
Andrey Masiero <andreymasiero@gmail.com>
2018-12-16 23:17:20 -0200

    Update on comments of first lesson.

--- src/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -6,7 +6,9 @@ object ValuesVariablesTypes extends App {
   val x : Int = 42
   println(x)
 
-  // A tipagem em Scala é opcional.
+  // A tipagem em Scala é opcional. O compilador infere o tipo.
+  // Se informar o tipo de maneira explicita o compilador acusará um erro
+  // quando este estiver errado
   val y = 42
   println(y)
 
