bf6606f5bfe928f8515409c980ec1a973de381f2
Andrey Masiero <andreymasiero@gmail.com>
2018-12-16 23:02:57 -0200

    First Commit

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,13 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  // Vals sao imutaveis. Similares as constantes e finals (C e Java)
+  val x : Int = 42
+  println(x)
+
+  // A tipagem em Scala é opcional.
+  val y = 42
+  println(y)
+
+}
